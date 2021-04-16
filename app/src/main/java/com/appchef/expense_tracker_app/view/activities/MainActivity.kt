1143ed5b6b556cddd47b0ae4ef443edb671afc07
Niket-Jain <52085669+Niket-Jain@users.noreply.github.com>
2021-04-16 22:21:17 +0530

    initial commit

---
+++ app/src/main/java/com/appchef/expense_tracker_app/view/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.appchef.expense_tracker_app.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.appchef.expense_tracker_app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/appchef/expense_tracker_app/view/activities/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.appchef.expense_tracker_app.view.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.appchef.expense_tracker_app.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        // Changing the Activity.
+        // Todo using Handler is not the best practice.
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+            finish()
+        },700)
+    }
+}
