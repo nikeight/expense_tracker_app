77bcfcf539e38b1b3563e94eab723f177310c69c
Yonit Chauhan <yonit833@gmail.com>
2021-04-17 16:47:29 +0530

    removed action bar updated bottom navigation bar

--- app/src/main/java/com/appchef/expense_tracker_app/view/activities/MainActivity.kt
+++ app/src/main/java/com/appchef/expense_tracker_app/view/activities/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.appchef.expense_tracker_app.view.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.NavController
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.appchef.expense_tracker_app.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -19,18 +16,6 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
-
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home,
-                R.id.navigation_expense_tracker,
-                R.id.navigation_add,
-                R.id.navigation_group_chat,
-                R.id.navigation_account
-            )
-        )
-
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
         navView.setupWithNavController(navController)
     }
