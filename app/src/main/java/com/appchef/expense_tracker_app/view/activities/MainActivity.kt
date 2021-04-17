ed0158f635ea42e32b89e04355d45d30c176f103
Yonit Chauhan <yonit833@gmail.com>
2021-04-17 18:40:20 +0530

    written layout for account fragment dark mode button now working

--- app/src/main/java/com/appchef/expense_tracker_app/view/activities/MainActivity.kt
+++ app/src/main/java/com/appchef/expense_tracker_app/view/activities/MainActivity.kt
@@ -14,9 +14,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
         val navController = findNavController(R.id.nav_host_fragment)
-
         navView.setupWithNavController(navController)
+
     }
 }
