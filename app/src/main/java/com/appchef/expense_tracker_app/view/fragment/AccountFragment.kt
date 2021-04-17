a62f0f31de1d04778e1a9375fba0b7fff4923a4c
Yonit Chauhan <yonit833@gmail.com>
2021-04-18 00:31:48 +0530

    implemented log out feature showing user details like name & number

--- app/src/main/java/com/appchef/expense_tracker_app/view/fragment/AccountFragment.kt
+++ app/src/main/java/com/appchef/expense_tracker_app/view/fragment/AccountFragment.kt
@@ -1,18 +1,23 @@
 package com.appchef.expense_tracker_app.view.fragment
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.appchef.expense_tracker_app.R
 import com.appchef.expense_tracker_app.databinding.FragmentAccountBinding
+import com.appchef.expense_tracker_app.view.activities.SplashScreen
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class AccountFragment : Fragment() {
 
-    // Binding Objects.
     private lateinit var binding: FragmentAccountBinding
 
     override fun onCreateView(
@@ -21,8 +26,23 @@ class AccountFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAccountBinding.inflate(inflater, container, false)
+        var user=FirebaseAuth.getInstance().currentUser
 
         setupDarkModeControls()
+
+        activity?.let {
+            Glide.with(it)
+                .load(user.photoUrl)
+                .placeholder(context?.let { it1 -> ContextCompat.getDrawable(it1,R.drawable.ic_account_vector) })
+                .into(binding.userImage)
+        }
+        binding.userName.text=user?.displayName ?: "User"
+        binding.userPhone.text=user?.phoneNumber ?: "1234567890"
+
+        binding.logOutBtn.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent(activity,SplashScreen::class.java))
+        }
 
         return binding.root
     }
